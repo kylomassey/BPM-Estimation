9d80aee03f8352c683def99e3a7e40b5625bc3e3
unknown <jmo4257@wgu.edu>
2026-05-26 19:57:58 -0400

    separtated visualization from spectrogram generation

--- BPM_estimation_proj/novelty_curve.py
+++ BPM_estimation_proj/novelty_curve.py
@@ -4,8 +4,11 @@ import librosa
 import matplotlib.pyplot as plt
 import numpy
 
-def onset(spectrum, hop_time):
+def onset_curve(spectrum, hop_time):
     spectrum =  numpy.mean(spectrum, axis=0)
     return numpy.maximum(numpy.diff(spectrum)/hop_time, 0)
 
-def smooth(onset, frame_len):
+def smooth_curve(onset, frame_len):
+    kernel = numpy.ones(frame_len, dtype=float)
+    smooth = numpy.convolve(onset, kernel, mode="same")
+    return smooth
---
+++ BPM_estimation_proj/visualization.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy
+
+def displaySpectrogram(spectrum, hop_time):
+    spectrum_db =  20 * numpy.log10(spectrum / numpy.max(spectrum) + 1e-10)
+    time_scale = numpy.arange(spectrum.shape[1]) * hop_time
+    freq_scale = numpy.arange(spectrum.shape[0]) * 20
+    spectrum_db = numpy.maximum(spectrum_db, -100)
+    plt.imshow(
+        spectrum_db,
+        origin = "lower",
+        aspect = "auto",
+        cmap = "magma",
+        extent = [time_scale[0], time_scale[-1], freq_scale[0], freq_scale[-1]]
+        )
+    plt.colorbar(label='dB')
+    plt.xlabel("Time frames")
+    plt.ylabel("Frequency bins")
+    plt.savefig("BPM_estimation_proj/charts/Spectrogram.png", dpi = 300, bbox_inches = "tight")
